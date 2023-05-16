31e7ab5b7ee7cefda07f47495036ac387e62d735
nascimentojon <127769948+nascimentojon@users.noreply.github.com>
2023-05-16 12:52:09 -0300

    16/05/2023

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import Navbar from "./componets/estaticos/navbar/Navbar";
+import Footer from "./componets/estaticos/footer/Footer";
+
+import './App.css'
+import Home from './paginas/home/Home'
+
+function App() {
+ 
+
+  return (
+    <>
+    <Navbar />
+    <Home />
+    <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Box, Paper, Button } from "@material-ui/core";
+import "./Home.css";
+
+
+function Home() {
+    return (
+        <div>
+         <h1> home</h1>
+        </div>
+    );
+}
+export default Home;
