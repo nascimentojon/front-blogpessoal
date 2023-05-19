9dd83ed4104b362f91e652b695596f990ec21c17
nascimentojon <127769948+nascimentojon@users.noreply.github.com>
2023-05-18 21:26:10 -0300

    login.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
-
+import React from 'react';
+import Login from "./paginas/login/Login";
 import Navbar from "./componets/estaticos/navbar/Navbar";
 import Footer from "./componets/estaticos/footer/Footer";
-
 import './App.css'
 import Home from './paginas/home/Home'
+import { BrowserRouter as Router, Route, Routes, } from "react-router-dom";
 
 function App() {
- 
+
 
   return (
-    <>
-    <Navbar />
-    <Home />
-    <Footer/>
-    </>
-  )
+    <Router>
+      <Navbar />
+      <div style={{ minHeight: "100vh" }}>
+      <Route  path="/">
+          <Login />
+        </Route>
+        <Routes>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        
+          <Route path="/home">
+            <Home />
+           </Route>
+           </Routes> 
+      </div>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App
--- src/componets/estaticos/footer/Footer.tsx
+++ src/componets/estaticos/footer/Footer.tsx
@@ -1,10 +1,42 @@
-import React from "react";
+import React from 'react';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import { Typography, Grid } from '@material-ui/core';
+import { Box } from '@mui/material';
 
-function Footer (){
-    return(
+function Footer() {
+    return (
         <>
-        <h1> texto</h1>
+            <Grid container direction="row" justifyContent="center" alignItems="center">
+                <Grid alignItems="center" item xs={12}>
+                    <Box style={{ backgroundColor: "#3F51B5", height: "120px" }}>
+                        <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
+                            <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
+                        </Box>
+                        <Box display="flex" alignItems="center" justifyContent="center">
+                            <a href="https://github.com/nascimentojon" target="_blank" rel="noopener noreferrer">
+                                <GitHubIcon style={{ fontSize: 60, color: "white" }} />
+                            </a>
+
+                            <a href="https://www.linkedin.com/in/jonatas-nascimento-3277a818a/" target="_blank" rel="noopener noreferrer">
+                                <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
+                            </a>
+                        </Box>
+                    </Box>
+                    <Box style={{ backgroundColor: "#303F9F", height: "60px" }}>
+                        <Box paddingTop={1}>
+
+                        </Box>
+                        <Box>
+                            <a target="_blank" href="https://brasil.generation.org" rel="noopener noreferrer">
+
+                            </a>
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
         </>
     )
 }
+
 export default Footer;
